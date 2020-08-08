dd68c2bec23ad50245f53d39291ae8fa2461dada
Richard Kasperowski <r@kasperowski.com>
2020-08-08 16:00:50 -0400

    Refactored for AIML

--- src/main/java/com/kasperowski/aichatbot/Chatbot.java
+++ src/main/java/com/kasperowski/aichatbot/Chatbot.java
@@ -1,35 +1,50 @@
 package com.kasperowski.aichatbot;
 
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.configuration.BotConfiguration;
+
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Chatbot {
+    private static Chat chat;
+
+    public Chatbot() {
+        Bot bot = new Bot(BotConfiguration.builder()
+                .name("alice")
+                .path("src/main/resources")
+                .build());
+        chat = new Chat(bot);
+    }
+
     public static void main(String[] args) throws IOException {
-        System.out.println(greeting());
+        Chatbot chatbot = new Chatbot();
+        System.out.println(chatbot.greeting());
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         //noinspection InfiniteLoopStatement
         while (true) {
             System.out.print("So now what? ");
             String input = reader.readLine();
-            System.out.println(respond(input));
+            System.out.println(chatbot.respond(input));
         }
     }
 
-    static String greeting() {
+    String greeting() {
         return "Hi there, I'm your chatbot!";
     }
 
-    static String respond(String input) {
-        if ("Hi".equals(input) || "hi".equals(input)) {
-            return "Well hello there!";
-        } else if ("Hello".equals(input) || "hello".equals(input)) {
-            return "Hello, my friend!";
-        } else if ("I want a cup of tea".equals(input)) {
-            return "Hmmm, I think you'll have to take care of that yourself - I'm just a chatbot.";
-        }
-        // else
-        return "Sorry, I don't understand. Will you try again?";
+    String respond(String input) {
+        return chat.multisentenceRespond(input);
+//        if ("Hi".equals(input) || "hi".equals(input)) {
+//            return "Well hello there!";
+//        } else if ("Hello".equals(input) || "hello".equals(input)) {
+//            return "Hello, my friend!";
+//        } else if ("I want a cup of tea".equals(input)) {
+//            return "Hmmm, I think you'll have to take care of that yourself - I'm just a chatbot.";
+//        }
+//        // else
+//        return "Sorry, I don't understand. Will you try again?";
     }
 }
--- src/test/java/com/kasperowski/aichatbot/ChatbotTests.java
+++ src/test/java/com/kasperowski/aichatbot/ChatbotTests.java
@@ -1,11 +1,20 @@
 package com.kasperowski.aichatbot;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ChatbotTests {
+    private Chatbot chatbot;
+
+    @BeforeEach
+    void setup() {
+        chatbot = new Chatbot();
+    }
+
     @Test
     void junitWorks() {
         assertTrue(true);
@@ -13,29 +22,31 @@ class ChatbotTests {
 
     @Test
     void greetsUs() {
-        String response = Chatbot.greeting();
+        String response = chatbot.greeting();
         assertEquals("Hi there, I'm your chatbot!", response);
     }
 
     @Test
     void hi() {
-        assertEquals("Well hello there!", Chatbot.respond("Hi"));
-        assertEquals("Well hello there!", Chatbot.respond("hi"));
+        assertEquals("Well hello there!", chatbot.respond("Hi"));
+        assertEquals("Well hello there!", chatbot.respond("hi"));
     }
 
     @Test
     void hello() {
-        assertEquals("Hello, my friend!", Chatbot.respond("Hello"));
-        assertEquals("Hello, my friend!", Chatbot.respond("hello"));
+        assertEquals("Hello, my friend!", chatbot.respond("Hello"));
+        assertEquals("Hello, my friend!", chatbot.respond("hello"));
     }
 
     @Test
-    void iWantTea() {
-        assertEquals("Hmmm, I think you'll have to take care of that yourself - I'm just a chatbot.", Chatbot.respond("I want a cup of tea"));
+    void iWantSomething() {
+        assertEquals("Hmmm, I think you'll have to take care of that yourself - I'm just a chatbot.", chatbot.respond("I want a cup of tea"));
+        assertEquals("Hmmm, I think you'll have to take care of that yourself - I'm just a chatbot.", chatbot.respond("I want a cup of tea."));
+        assertEquals("Hmmm, I think you'll have to take care of that yourself - I'm just a chatbot.", chatbot.respond("I want a lobster roll and some ice cream."));
     }
 
     @Test
     void garbageIn() {
-        assertEquals("Sorry, I don't understand. Will you try again?", Chatbot.respond("random junk asunthaoeusntoehu"));
+        assertEquals("Sorry, I don't understand. Will you try again?", chatbot.respond("random junk asunthaoeusntoehu"));
     }
 }
